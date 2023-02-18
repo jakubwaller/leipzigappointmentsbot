b0340f91214421f56a1b1b6a5d8ec18a0c3f5b37
Jakub Waller <jakub.waller@protonmail.com>
2023-02-18 07:47:14 -0300

    thank you twitter

--- leipzigappointmentsbot.py
+++ leipzigappointmentsbot.py
@@ -12,14 +12,14 @@ chat_id = config["chat_id"]
 wsid = config["wsid"]
 
 try:
-    auth = tweepy.OAuth1UserHandler(
-        config['twitter_key'],
-        config['twitter_secret'],
-        config['twitter_token_key'],
-        config['twitter_token_secret']
-    )
-
-    api = tweepy.API(auth)
+    # auth = tweepy.OAuth1UserHandler(
+    #     config['twitter_key'],
+    #     config['twitter_secret'],
+    #     config['twitter_token_key'],
+    #     config['twitter_token_secret']
+    # )
+    #
+    # api = tweepy.API(auth)
 
     appointments = run_request("GET",
                                'https://terminvereinbarung.leipzig.de/m/leipzig-ba/extern/calendar/search_result?'
@@ -46,24 +46,24 @@ try:
                        f"{collected_date} {collected_time} - {collected_unit}"
     if full_message != "":
         send_message(bot_id, chat_id, full_message, num_of_tries=1, timestamp=False)
-        if len(full_message) <= 280:
-            api.update_status(full_message)
-        else:
-            last_newline = full_message.rfind("\n", 0, 280)
-            if last_newline == -1:
-                last_newline = 280
-            tweet = api.update_status(full_message[0:last_newline])
-            full_message = full_message[last_newline:]
-            remaining_length = len(full_message)
-            while remaining_length > 0:
-                last_newline = full_message.rfind("\n", 0, 280)
-                if last_newline == -1 or len(full_message) <= 280:
-                    last_newline = 280
-                tweet = api.update_status(status=full_message[0:last_newline],
-                                          in_reply_to_status_id=tweet.id,
-                                          auto_populate_reply_metadata=True)
-                full_message = full_message[last_newline:]
-                remaining_length = len(full_message)
+        # if len(full_message) <= 280:
+        #     api.update_status(full_message)
+        # else:
+        #     last_newline = full_message.rfind("\n", 0, 280)
+        #     if last_newline == -1:
+        #         last_newline = 280
+        #     tweet = api.update_status(full_message[0:last_newline])
+        #     full_message = full_message[last_newline:]
+        #     remaining_length = len(full_message)
+        #     while remaining_length > 0:
+        #         last_newline = full_message.rfind("\n", 0, 280)
+        #         if last_newline == -1 or len(full_message) <= 280:
+        #             last_newline = 280
+        #         tweet = api.update_status(status=full_message[0:last_newline],
+        #                                   in_reply_to_status_id=tweet.id,
+        #                                   auto_populate_reply_metadata=True)
+        #         full_message = full_message[last_newline:]
+        #         remaining_length = len(full_message)
 except Exception as exc:
     error_message = str(exc)[0:100]
     if "Status is a duplicate" not in error_message:
